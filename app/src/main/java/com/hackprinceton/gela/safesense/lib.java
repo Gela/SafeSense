2d143310b2b91812427bafd6aa9ab1631a1d8999
Gela <Gelarehm@buffalo.edu>
2015-04-18 17:29:56 -0400

    added Google Play support in the gradle

---
+++ app/src/main/java/com/hackprinceton/gela/safesense/lib.java
@@ -0,0 +1,25 @@
+
+
+package com.hackprinceton.gela.safesense;
+
+/**
+ * Created by Gela on 4/18/2015.
+ */
+public class lib {
+
+    int currentSpeed;
+    int speed;
+
+    private int speedCalculator(){
+        location.getSpeed();
+    }
+    public int getCurrentSpeed() {
+        //returns the current speed infinitely
+        return currentSpeed;
+    }
+
+    public int setCurrentSpeed(int currentSpeed) {
+        //sets the current speed
+        speed = currentSpeed;
+    }
+}
